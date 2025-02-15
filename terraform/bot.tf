866aa01007128a42d0682f92c6574ba07f7ea808
Polina Ryftina <pryftina@gmail.com>
2025-02-16 00:18:05 +0300

    Донастройка тераформа

--- terraform/bot.tf
+++ terraform/bot.tf
@@ -29,3 +29,56 @@ resource "yandex_function" "bot" {
     }
   }
 }
+
+resource "yandex_function_iam_binding" "exam_solver_tg_bot_iam" {
+  function_id = yandex_function.bot.id
+  role        = "functions.functionInvoker"
+  members = [
+    "system:allUsers",
+  ]
+}
+
+resource "telegram_bot_webhook" "exam_solver_tg_bot_webhook" {
+  url = "https://functions.yandexcloud.net/${yandex_function.bot.id}"
+}
+
+resource "yandex_api_gateway" "api_gw" {
+  name = var.api_gateway
+  spec = <<-EOT
+openapi: "3.0.0"
+info:
+  version: 1.0.0
+  title: Photo Face Detector API
+paths:
+  /:
+    get:
+      summary: Serve face images from Yandex Cloud Object Storage
+      parameters:
+        - name: face
+          in: query
+          required: true
+          schema:
+            type: string
+      x-yc-apigateway-integration:
+        type: object_storage
+        bucket: ${yandex_storage_bucket.faces_bucket.bucket}
+        object: "{face}"
+        service_account_id: ${yandex_iam_service_account.sa_bot.id}
+EOT
+}
+
+data "archive_file" "bot_source" {
+  type        = "zip"
+  source_dir  = "../src/bot"
+  output_path = "../build/bot.zip"
+}
+
+resource "yandex_iam_service_account" "sa_bot" {
+  name = var.sa_bot
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "sa_bot_storage_editor_iam" {
+  folder_id = var.folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa_bot.id}"
+}
--- terraform/cropper.tf
+++ terraform/cropper.tf
@@ -33,3 +33,49 @@ resource "yandex_function" "cropper" {
     }
   }
 }
+
+resource "yandex_function_trigger" "crop_tasks_queue_trigger" {
+  name = var.face_cut_trigger
+  function {
+    id                 = yandex_function.cropper.id
+    service_account_id = yandex_iam_service_account.sa_cropper.id
+  }
+  message_queue {
+    batch_cutoff       = "0"
+    batch_size         = "1"
+    queue_id           = yandex_message_queue.crop_tasks_queue.arn
+    service_account_id = yandex_iam_service_account.sa_cropper.id
+  }
+}
+
+data "archive_file" "cropper_source" {
+  type        = "zip"
+  source_dir  = "../src/cropper"
+  output_path = "../build/cropper.zip"
+}
+
+resource "yandex_iam_service_account" "sa_cropper" {
+  name = var.sa_face_cut
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "sa_cropper_function_invoker_role" {
+  folder_id = var.folder_id
+  role      = "functions.functionInvoker"
+  member    = "serviceAccount:${yandex_iam_service_account.sa_cropper.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "sa_cropper_storage_editor_iam" {
+  folder_id = var.folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa_cropper.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "sa_cropper_ymq_reader_iam" {
+  folder_id = var.folder_id
+  role      = "ymq.reader"
+  member    = "serviceAccount:${yandex_iam_service_account.sa_cropper.id}"
+}
+
+resource "yandex_iam_service_account_static_access_key" "sa_cropper_static_key" {
+  service_account_id = yandex_iam_service_account.sa_cropper.id
+}
